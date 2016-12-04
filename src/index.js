eccfd82c37500db9a60ebe085a16a5066bda654d
Terrence Lam <skyuplam@gmail.com>
2016-12-04 09:26:18 +0100

    Add travis-ci

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import { compose  } from './compose';
+import compose from './compose';
+import applyMiddleware from './applyMiddleware';
+import createStore from './createStore';
 
 export {
-    compose,
+  compose,
+  applyMiddleware,
+  createStore,
 };
