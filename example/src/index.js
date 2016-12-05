c82e03244e83a363c00619574e47edd7e49f5b5e
Terrence Lam <skyuplam@gmail.com>
2016-12-05 10:04:53 +0100

    Add comments

--- example/src/index.js
+++ example/src/index.js
@@ -10,26 +10,31 @@ import {
   fetchUsers,
 } from './actions';
 
+// initiate saga middleware
 const sagaMiddleware = createSagaMiddleware();
 const middleware = [sagaMiddleware];
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
+// create redux store and apply saga middleware
 const store = createStore(
   reducer,
   {},
   applyMiddleware(...middleware),
 );
 
+// run saga runtime
 store.runSaga = sagaMiddleware.run;
 appSagas.map(sagaMiddleware.run);
 
+// fetch users from github
 store.dispatch(fetchUsers());
 
+// render the page
 const render = () => ReactDOM.render(
   <UserList users={store.getState().users}/>,
   document.getElementById('root')
 );
 
 render();
+
+// listen the change from redux store
 store.subscribe(render);
